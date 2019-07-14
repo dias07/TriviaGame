830540da13789a667c9f5d2e709a889d077d78ff
Diyas Asset <diyasasset@diyass-mbp.attlocal.net>
2019-07-13 22:42:14 -0500

    I finished main things, left a little

---
+++ assets/javascript/app.js
@@ -0,0 +1,129 @@
+$( document ).ready(function() {
+    var allQuestions = ["How many colours are there in a rainbow?","Whose statue in Red Square was pulled down in 1991?",
+        "Who was the very first female member of Parliament in Britain? ", "What was Che Guevara's nationality?"];
+    var allAnswers = ["7","Lenin's","Nancy Astor","Argentinian"];
+   
+    var chAnswers =[
+        ["2","7","3","5"],
+        ["Lenin's","Stalin's", "Roosevelt's", "Shakira's"],
+        ["Marie Curie","Nancy Astor", "Marilyn Monroe","Queen Tomiri"],
+        ["Russian", "Iranian", "Mexican", "Argentinian"] 
+        ];
+    var chAnswersreally = [];
+    var gamestart=true;
+    var myImage = new Image(300, 200);
+    var srcOfImages = ['assets/images/rainbow.gif',
+    'assets/images/lenin.gif',
+    'assets/images/NancyAstor.jpg',
+    'assets/images/Argentina.gif',
+    ];
+    var correctAnswer = "";
+    var buttoncick1;
+    var time;
+    var intervalId;
+    var Qincrease = 0;
+    
+
+
+    function run() {
+        time = 31;
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+      }
+
+
+      function decrement() {
+        time--;
+        $("#Time").html("Time Remaining:" + time + "Seconds");
+        if (time === 0) {
+            for (var j=0; j<buttoncick1.length; j++)  {
+                if (jQuery.inArray(buttoncick1[j],allAnswers)>=0){
+                    $("#correctAnswer").html("The Correct Answer was:" + buttoncick1[j])}
+                    $("#questionandanswers").hide();
+                    $("#images").show();
+                    $("#loadimage").prepend(myImage);  
+                    stop();
+                }
+        }
+      }
+      function stop() {
+
+        //  Clears our intervalId
+        //  We just pass the name of the interval
+        //  to the clearInterval function.
+        clearInterval(intervalId);
+      }
+  
+      //  Execute the run function.
+      run();
+      increase ();
+
+    function increase (){
+        $("#question").html(allQuestions[Qincrease]);
+        Qincrease ++;
+        if (Qincrease == 4){
+            Qincrease=0;
+        }
+        
+    }
+       
+
+    
+    function setquestions (ProbableQuestions){
+        for (var i=0; i<ProbableQuestions.length;i++){
+            if (document.getElementById("question").innerHTML==ProbableQuestions[i])
+           {
+            $("#first").html(chAnswers[i][0]);
+            $("#second").html(chAnswers[i][1]);
+            $("#third").html(chAnswers[i][2]);
+            $("#fourth").html(chAnswers[i][3]);
+            myImage.src=srcOfImages[i];  
+            document.getElementById("first").value=chAnswers[i][0];
+            document.getElementById("second").value=chAnswers[i][1];
+            document.getElementById("third").value=chAnswers[i][2];
+            document.getElementById("fourth").value=chAnswers[i][3];
+                buttoncick1=chAnswers[i];
+                i++;
+           }
+        }
+    }
+    setquestions (allQuestions);
+
+
+    $('.allbuttons').on('click', function(event){
+       if (jQuery.inArray(event.target.value, allAnswers )>=0){
+           $("#questionandanswers").hide();
+           $("#images").show();
+           $("#loadimage").prepend(myImage);  
+           $("#correctAnswer").append("</br>Correct!");
+           stop();
+       }
+       else {
+           for (var j=0; j<buttoncick1.length; j++)  {
+            if (jQuery.inArray(buttoncick1[j],allAnswers)>=0){
+                $("#correctAnswer").html("The Correct Answer was:" + buttoncick1[j])}
+                $("#questionandanswers").hide();
+                $("#images").show();
+                $("#loadimage").prepend(myImage);  
+                stop();
+            }
+       }
+       
+          
+        
+    })
+    $("#loadimage").on('click', function(){
+        $("#questionandanswers").show();
+        $("#images").hide();
+        run();
+        increase ();
+        setquestions (allQuestions);
+    })
+    
+    
+
+
+
+
+
+});
